3bc6bb1cfa1414cd4e9e6ef2496ba1a2b6777958
Victor Olofsson <victor__o@hotmail.com>
2020-01-03 10:48:53 +0100

    changes

--- test.py
+++ test.py
@@ -3,46 +3,51 @@ from node import node
 import argparse
 import time
 import sys
+import threading
 
-APP_NAME = "Ra_Test"
 
 
-def parseArgs():
-    parser = argparse.ArgumentParser(prog=APP_NAME, usage='%(prog)s [options]')
-    parser.add_argument('--sponsor_addr','-r', type=str, required = False, default = '127.0.0.1', help='sponsor address')
-    parser.add_argument('--sponsor_port','-i', type=int, default = None, help='sponsor port')
-    parser.add_argument('--use_time','-u',type=int,required	= True, help="Using resource time")
-    parser.add_argument('--wait_time','-w',type=int,required = True, help="Idle time")
-    parser.add_argument('--name','-n',type=str,required =True, help="Unique Node Name")
-    parser.add_argument('--addr','-a',type=str,required =False, default = '', help="node address")
-    parser.add_argument('--port','-p',type=int,required =False, default = 0, help="node port")
-    return parser.parse_args()
+# def __init__(self,args):
+#     self.use_time = args.use_time
+#     self.wait_time = args.wait_time
+#     self.name = args.name
+#     self.sponsor = (args.sponsor_addr, args.sponsor_port)
+#     self.addr = args.addr
+#     self.port = args.port
 
-class RaTest(object):
-    def __init__(self,args):
-        self.use_time = args.use_time
-        self.wait_time = args.wait_time
-        self.name = args.name
-        self.sponsor = (args.sponsor_addr, args.sponsor_port)
-        self.addr = args.addr
-        self.port = args.port
+def runTest():
+    nodeA = node("Node_A","0.0.0.0", 5000)
+    nodeB = node("Node_B","0.0.0.0", 5001)
+    nodeC = node("Node_C","0.0.0.0", 5002)
 
-    def runTest(self):
-        test = node(self.name,self.addr, self.port)
-        
-        if (self.sponsor[0] != None ) and (self.sponsor[1] != None):
-            print("Node ::" + self.name + "::INITIALIZATION")
-            test.init(self.sponsor)
-        print("NODE::" + self.name + "::READY")
-        
-        time.sleep(self.wait_time)
-        test.acquire()
-        print(self.name)
-        time.sleep(2)
-        test.release()
-        time.sleep(10)
 
+    threadB = threading.Thread(target = programB)
+    threadC = threading.Thread(target = programC)
+    threadA = threading.Thread(target = programA)
+
+def programA(self, node):
+    node.acquire()
+    print("proc_a")
+    time.sleep(2)
+    node.release()
+    time.sleep(10)
+
+def programB(self, node):
+    time.sleep(5)
+    node.acquire()
+    print("proc_b")
+    time.sleep(2)
+    node.release()
+    time.sleep(10)
+
+def programC(self, node):
+    time.sleep(6)
+    node.acquire()
+    print("proc_c")
+    time.sleep(2)
+    node.release()
+    
+    
 
 if __name__ == "__main__":
-    test = RaTest(parseArgs())
-    test.runTest()
+    runTest()
